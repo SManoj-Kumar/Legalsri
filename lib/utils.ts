69abb735a8b6192fb2306f74572a1dfb32999c07
Dev <pattnaikd833@gmail.com>
2026-03-11 20:38:51 +0530

    feat: introduce utility functions for class merging and asset path generation, adding `clsx` and `tailwind-merge` dependencies.

---
+++ lib/utils.ts
@@ -0,0 +1,15 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function getImagePath(path: string) {
+  return `${path.startsWith("/") ? "" : "/"}${path}`
+}
+
+export function getAssetPath(path: string) {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""
+  return `${basePath}${path.startsWith("/") ? "" : "/"}${path}`
+}
